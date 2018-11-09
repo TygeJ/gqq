e1c49593f03e643dbd5d51f721df2303e4dcd107
Tyge Johnson <tygesjohnson@gmail.com>
2018-11-09 07:35:30 -0700

    Delete demoToken.js

--- demoToken.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://connect.podium.com/widget.js#API_TOKEN=12794e7b37b79815188f59eb044d42ec';
-script.id="podium-widget"
-document.head.appendChild(script);
